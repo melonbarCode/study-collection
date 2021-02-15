b7857ef5d2d0f7288d09a8018fef7ddfb4e5a117
melonBarCode <melonbarcode@gmail.com>
2021-02-15 23:14:37 +0900

    sort h-index update

--- algorithm/programmers/sort/bigNum.js
+++ algorithm/programmers/sort/bigNum.js
@@ -1,14 +1,17 @@
-
 /**
- * 가장큰수 
+ * 가장큰수
  * https://programmers.co.kr/learn/courses/30/lessons/42746
- * @param {*} numbers 
+ * @param {*} numbers
  */
 function solution(numbers = []) {
+  
+  const answer = numbers
+    .map((n) => String(n))
+    .sort((a, b) => b + a - (a + b))
+    .join("") 
 
-    return numbers.map(n => String(n)).sort((a,b)=> (b+a) - (a+b)).join("")
-    
+    return answer[0] === "0" ? "0" : answer
 }
 
 // console.log(solution([6, 10, 2]))
-console.log(solution([3, 30, 34, 5, 9]	))
+console.log(solution([3, 30, 34, 5, 9]));
--- algorithm/programmers/sort/hIndex.js
+++ algorithm/programmers/sort/hIndex.js
@@ -1,4 +1,20 @@
+
+/**
+ * 
+ * https://programmers.co.kr/learn/courses/30/lessons/42747
+ * H-Index
+ * @param {*} citations 
+ */
 function solution(citations) {
-    var answer = 0;
-    return answer;
+    citations.sort((a,b)=> b-a);
+
+    let hIndex = 0;
+
+    while(hIndex + 1 <= citations[hIndex]){
+        hIndex++;
+    }
+
+    return hIndex;
 }
+
+console.log(solution([3, 0, 6, 1, 5]))
