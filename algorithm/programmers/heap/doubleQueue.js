b2605556e2d2ccfb2c4c980c4d7ea963aed901a1
melonBarCode <melonbarcode@gmail.com>
2021-02-23 22:44:41 +0900

    test

--- algorithm/programmers/heap/doubleQueue.js
+++ algorithm/programmers/heap/doubleQueue.js
@@ -12,10 +12,15 @@ function solution(operations) {
     switch (command) {
       case "I":
         queue.push(+number);
-        queue.sort((a, b) => a - b);
         break;
       case "D":
-        number === "1" ? queue.pop() : queue.shift();
+        if (queue.length === 0) break;
+
+        number === "1"
+          ? queue.splice(queue.indexOf(Math.max.apply(null, queue)), 1)
+          : queue.splice(queue.indexOf(Math.min.apply(null, queue)), 1);
+
+        console.log(queue);
         break;
       default:
         break;
@@ -26,7 +31,53 @@ function solution(operations) {
     return [0, 0];
   }
 
+  queue.sort((a, b) => a - b);
+
   return [queue[queue.length - 1], queue[0]];
 }
 
-console.log(solution(["I 7", "I 5", "I -5", "D -1"]));
+console.log(
+  solution([
+    "I -45",
+    "I 653",
+    "D 1",
+    "I -642",
+    "I 45",
+    "I 97",
+    "D 1",
+    "D -1",
+    "I 333",
+  ])
+);
+
+
+
+// console.log(solution(["I 7", "I 5", "I -5", "D -1"]));//
+
+
+// 전 다른 풀이
+// function solution(operations) {
+//   const queue = [];
+
+//   for (let oper of operations) {
+//     const [command, number] = oper.split(" ");
+
+//     switch (command) {
+//       case "I":
+//         queue.push(+number);
+//         queue.sort((a, b) => a - b);
+//         break;
+//       case "D":
+//         number === "1" ? queue.pop() : queue.shift();
+//         break;
+//       default:
+//         break;
+//     }
+//   }
+
+//   if (queue.length === 0) {
+//     return [0, 0];
+//   }
+
+//   return [queue[queue.length - 1], queue[0]];
+// }
