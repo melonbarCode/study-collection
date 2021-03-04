42e6ed5aca821d6ca6759ae30a4235c810d52f9a
melonBarCode <melonbarcode@gmail.com>
2021-03-05 01:03:51 +0900

    network dfs

--- algorithm/programmers/dfsbfs/network.js
+++ algorithm/programmers/dfsbfs/network.js
@@ -1,4 +1,36 @@
-function solution(n, computers) {
-    var answer = 0;
-    return answer;
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/43162
+ * @param {*} n
+ * @param {*} computers
+ */
+function solution(n, computers = [[]]) {
+  let answer = 0;
+  const visited = [];
+
+  for (let i = 0; i < n; i++) {
+    if (!visited[i]) {
+      answer++;
+      dfs(i, computers, visited);
+    }
+  }
+
+  function dfs(start, computers, visited) {
+    visited[start] = true;
+
+    for (let i = 0; i < computers.length; i++) {
+      if (!visited[i] && computers[start][i]) {
+        dfs(i, computers, visited);
+      }
+    }
+  }
+
+  return answer
 }
+
+console.log(
+  solution(3, [
+    [1, 1, 0],
+    [1, 1, 0],
+    [0, 0, 1],
+  ])
+);
