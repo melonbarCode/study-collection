2194fb38954fc47d170518e7f4a683d71e5edd11
melonbarCode <melonbarcode@gmail.com>
2022-04-28 01:03:41 +0900

    feat: inflearn section 6-3 올바른괄호

---
+++ algorithm/inflean/javascript-algorithm/section6/3.correctCurl.js
@@ -0,0 +1,43 @@
+/**
+*
+올바른 괄호
+괄호가 입력되면 올바른 괄호이면 “YES", 올바르지 않으면 ”NO"를 출력합니다.
+(())() 이것은 괄호의 쌍이 올바르게 위치하는 거지만, (()()))은 올바른 괄호가 아니다.
+▣ 입력설명
+첫 번째 줄에 괄호 문자열이 입력됩니다. 문자열의 최대 길이는 30이다.
+▣ 출력설명
+첫 번째 줄에 YES, NO를 출력한다.
+
+▣ 입력예제 1
+(()(()))(()
+▣ 출력예제 1
+NO
+
+ */
+
+//나의 문제풀이
+function solution(text = "") {
+  let answer = "YES";
+
+  let stack = [];
+
+  for (let t of text) {
+    if (t === "(") {
+      stack.push(t);
+    }
+
+    if (t === ")") {
+      if (stack.length > 0) stack.pop();
+      else return "NO";
+    }
+  }
+
+  if (stack.length !== 0) {
+    return "NO";
+  }
+
+  return answer;
+}
+
+//YES
+console.log(solution("(()(()))(()"));
